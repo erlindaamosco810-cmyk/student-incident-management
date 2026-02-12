5ed2cbe47c68c92d595fedc5e5ee8c51aa41f457
erlindaamosco810-cmyk <erlindaamosco810@gmail.com>
2026-02-12 08:40:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// main.js
+
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  <h1>Welcome to Student Incident Management</h1>
+  <p>This is your Vite project!</p>
+`;
+
+// Example: you can add more JS logic here
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',          // root folder ng index.html
+  build: {
+    outDir: 'dist',   // publish directory
+  },
+})
